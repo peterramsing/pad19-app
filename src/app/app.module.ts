c6a8033af772740842aae3ea39d3fa8a0ec47e04
Peter Ramsing <peter.ramsing@mac.com>
2017-10-23 22:37:04 -0700

    Adds the ability to change the title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
@@ -40,7 +40,10 @@ const appRoutes: Routes = [
     ),
     CoreModule,
   ],
-  providers: [AuthGuard],
+  providers: [
+    AuthGuard,
+    Title,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
